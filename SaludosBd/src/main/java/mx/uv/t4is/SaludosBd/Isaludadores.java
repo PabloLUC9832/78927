2019152ed8bf84610a4dc48751a301f23188fd53
PabloLUC9832 <lucaspablo340@outlook.com>
2022-03-29 12:52:31 -0600

    Spring con bd, metodo saludar

---
+++ SaludosBd/src/main/java/mx/uv/t4is/SaludosBd/Isaludadores.java
@@ -0,0 +1,10 @@
+package mx.uv.t4is.SaludosBd;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Isaludadores extends CrudRepository<Saludadores,Integer>{
+    
+    
+
+
+}
