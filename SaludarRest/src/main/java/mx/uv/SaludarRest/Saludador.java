0fb346dc4ca80c9327647ff6ef5d2ba3db3d4ca7
PabloLUC9832 <lucaspablo340@outlook.com>
2022-05-03 06:05:54 +0000

    Ejercicio Saludarest, clase saludador

---
+++ SaludarRest/src/main/java/mx/uv/SaludarRest/Saludador.java
@@ -0,0 +1,14 @@
+package mx.uv.SaludarRest;
+
+public class Saludador{
+
+  private String contenido;
+
+  public Saludador(String contenido){
+    this.contenido = contenido;
+  }
+
+  public String getContenido(){
+    return contenido;
+  }
+}
--- SaludarRest/src/main/java/mx/uv/SaludarRest/SaludarControlador.java
+++ SaludarRest/src/main/java/mx/uv/SaludarRest/SaludarControlador.java
@@ -3,6 +3,8 @@ package mx.uv.SaludarRest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class SaludarControlador{
@@ -21,6 +23,15 @@ public class SaludarControlador{
     return "hola desde /saludar POST";
   }
 
+  @GetMapping("/saludar1/{nombre}")
+  public Saludador saludarPath1(@PathVariable String nombre){
+    return new Saludador(nombre);
+  }
+
+  @RequestMapping(value="/saludar2/{nombrep}",method=RequestMethod.GET)
+  public String saludarPath2(@PathVariable("nombrep") String nombre){
+    return "Hola nombrep: "+ nombre;
+  }
 
 }
 
