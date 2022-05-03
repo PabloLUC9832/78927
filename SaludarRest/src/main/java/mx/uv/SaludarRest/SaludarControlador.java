39f3a0d0335de97d718491182bf981d5b06ec686
PabloLUC9832 <lucaspablo340@outlook.com>
2022-05-03 11:58:13 -0500

    se agrego saludarme1,saludarme2

--- SaludarRest/src/main/java/mx/uv/SaludarRest/SaludarControlador.java
+++ SaludarRest/src/main/java/mx/uv/SaludarRest/SaludarControlador.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class SaludarControlador{
@@ -27,11 +29,21 @@ public class SaludarControlador{
   public Saludador saludarPath1(@PathVariable String nombre){
     return new Saludador(nombre);
   }
-
+  //a
   @RequestMapping(value="/saludar2/{nombrep}",method=RequestMethod.GET)
   public String saludarPath2(@PathVariable("nombrep") String nombre){
     return "Hola nombrep: "+ nombre;
   }
+
+  @GetMapping("/saludarme1")
+  public Saludador saludarMe1(@RequestParam String nombre){
+    return new Saludador(nombre);
+  }  
+
+  @GetMapping("/saludarme2")
+  public String saludarMe2(@RequestParam(name="nombre",defaultValue=" mundo!!") String nombre){
+    return "Hola nombreQueryString: "+nombre;
+  }  
 
 }
 
